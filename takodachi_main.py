db4dbdf7c4f614ac1e7cbca6b2279b2827773df3
chibi-chichi <78858443+chibi-chichi@users.noreply.github.com>
2021-09-12 23:45:56 +0900

    Update takodachi_main.py

--- takodachi_main.py
+++ takodachi_main.py
@@ -1,9 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
-
-win = Tk()
-win.title("TAKODACHI")
+import channel_info as ch
 
 '''
 넣을 기능
@@ -18,17 +16,87 @@ win.title("TAKODACHI")
 5. 홀로라이브 공식 트윗 크롤링
 '''
 
-config = Menu(win)
-win.config(menu=config)
 
+# 언어 변경 기능
 def language_change():
-    messagebox.showinfo("언어 변경","언어 변경을 하고 싶다.")
+    messagebox.showinfo("언어 변경", "언어 변경을 하고 싶다.")
 
-config_menu = Menu(config)
-config.add_cascade(label="설정", menu = config_menu)
-config_menu.add_command(label="언어 변경", command=language_change)
-config_menu.add_separator()
 
-config_menu.add_command(label="선호 캐릭터")
+count = 0
 
-win.mainloop()
+
+def count_up(button):
+    global count
+    count += 1
+    button.config(text=str(count))
+
+
+# 와! 기존 프레임에서 새로운 프레임 띄우기! 이런게 있네
+# https://www.delftstack.com/ko/howto/python-tkinter/how-to-switch-frames-in-tkinter/
+class TakoDachiApp(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.title("TAKODACHI")
+        # 초기 화면 크기 설정 (1280X720)
+        self.geometry("1280x720")
+        # 창 크기 조절 (X축, Y축)
+        self.resizable(False, False)
+        self._frame = None
+        self.switch_frame(StartPage)
+
+    # 프레임 스위치 기능 (기존의 프레임을 지우고 새 프레임을 생성)
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+
+# 처음 실행 페이지
+class StartPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        Label(self, text="Start page", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+        Button(self, text="Go to page one",
+               command=lambda: master.switch_frame(PageOne)).pack()
+        Button(self, text="Go to page two",
+               command=lambda: master.switch_frame(PageTwo)).pack()
+        Button(self, text="Go to page three",
+               command=lambda: master.switch_frame(PageThree)).pack()
+
+
+# 타코다치 페이지
+class PageOne(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        Frame.configure(self, bg='blue')
+        Label(self, text="타코 페이지 입니다!", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+        Button(self, text="Go back to start page",
+               command=lambda: master.switch_frame(StartPage)).pack()
+
+
+# 스이쨩 페이지
+class PageTwo(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        Frame.configure(self, bg='red')
+        Label(self, text="스이쨩 페이지 입니다!", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+        Button(self, text="Go back to start page",
+               command=lambda: master.switch_frame(StartPage)).pack()
+
+
+# 칼리 페이지
+class PageThree(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        Frame.configure(self, bg='green')
+        Label(self, text="칼리 페이지 입니다!", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+        Button(self, text="Go back to start page",
+               command=lambda: master.switch_frame(StartPage)).pack()
+
+
+# 프로그램이 종료될 때 까지 실행
+if __name__ == "__main__":
+    app = TakoDachiApp()
+    app.mainloop()
